3ae0909880f50cf3b8825fa08b86d161841b5097
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2015-02-09 12:19:42 +0100

    fff

--- trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/mapmatch/IMapMatcher.java
+++ trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/mapmatch/IMapMatcher.java
@@ -1,11 +1,15 @@
 package de.uniol.inf.is.odysseus.trajectory.physical.compare.uots.mapmatch;
 
+import com.vividsolutions.jts.geom.LineSegment;
+import com.vividsolutions.jts.geom.Point;
+
 import de.uniol.inf.is.odysseus.trajectory.physical.compare.RawTrajectory;
 import de.uniol.inf.is.odysseus.trajectory.physical.compare.uots.UotsTrajectory;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 
 
 public interface IMapMatcher {
 
 	
-	public UotsTrajectory map(final RawTrajectory trajectory);
+	public UotsTrajectory map(final RawTrajectory trajectory, UndirectedSparseGraph<Point, LineSegment> graph);
 }
--- trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/mapmatch/PointToPointMapMatcher.java
+++ trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/mapmatch/PointToPointMapMatcher.java
@@ -1,13 +1,44 @@
 package de.uniol.inf.is.odysseus.trajectory.physical.compare.uots.mapmatch;
 
+import java.util.Iterator;
+
+import com.vividsolutions.jts.geom.LineSegment;
+import com.vividsolutions.jts.geom.Point;
+
 import de.uniol.inf.is.odysseus.trajectory.physical.compare.RawTrajectory;
 import de.uniol.inf.is.odysseus.trajectory.physical.compare.uots.UotsTrajectory;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 
 public class PointToPointMapMatcher implements IMapMatcher {
 
-	@Override
+	
 	public UotsTrajectory map(RawTrajectory trajectory) {
 		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UotsTrajectory map(RawTrajectory trajectory,
+			UndirectedSparseGraph<Point, LineSegment> graph) {
+		
+		//ersten finden
+		final Iterator<Point> rawIt = trajectory.getPoints().iterator();
+		Point rawPoint = rawIt.next();
+		
+		final Iterator<Point> graphIt = graph.getVertices().iterator();
+		Point minGraphPoint = graphIt.next();
+		double minDistance = rawPoint.distance(minGraphPoint);
+		
+		while(graphIt.hasNext()) {
+			Point nextGraphPoint = graphIt.next();
+			final double nextDistance = rawPoint.distance(nextGraphPoint);
+			if(nextDistance < minDistance) {
+				minGraphPoint = nextGraphPoint;
+				minDistance = nextDistance;
+			}
+		}
+		
+		
 		return null;
 	}
 

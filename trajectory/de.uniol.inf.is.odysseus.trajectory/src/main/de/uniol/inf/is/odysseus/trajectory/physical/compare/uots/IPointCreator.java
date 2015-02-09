c35f1fc9e825e8727dea51f7c66563a3cb24543d
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2015-02-09 11:38:46 +0100

    ffff

--- trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/IPointCreator.java
+++
@@ -1,8 +0,0 @@
-package de.uniol.inf.is.odysseus.trajectory.physical.compare.uots;
-
-import com.vividsolutions.jts.geom.Point;
-
-public interface IPointCreator {
-
-	public Point createPoint(double x, double y);
-}
--- trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/physical/compare/uots/UotsTrajectory.java
+++
@@ -1,5 +0,0 @@
-package de.uniol.inf.is.odysseus.trajectory.physical.compare.uots;
-
-public class UotsTrajectory {
-
-}

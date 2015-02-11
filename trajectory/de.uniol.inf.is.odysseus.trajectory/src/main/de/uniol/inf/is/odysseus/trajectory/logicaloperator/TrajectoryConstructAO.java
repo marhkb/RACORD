57345c0d3899d141ee2a4f969b3ebc022b1ddc57
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2015-02-11 11:35:41 +0100

    aa

--- trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/logicaloperator/TrajectoryConstructAO.java
+++ trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/logicaloperator/TrajectoryConstructAO.java
@@ -10,7 +10,10 @@ import de.uniol.inf.is.odysseus.core.server.logicaloperator.UnaryLogicalOp;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.LogicalOperator;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.annotations.Parameter;
 import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.BooleanParameter;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.ResolvedSDFAttributeParameter;
 import de.uniol.inf.is.odysseus.spatial.sourcedescription.sdf.schema.SDFSpatialDatatype;
+import de.uniol.inf.is.odysseus.trajectory.logicaloperator.builder.NestAggregateItem;
+import de.uniol.inf.is.odysseus.trajectory.logicaloperator.builder.NestAggregateItemParameter;
 
 @LogicalOperator(name = "TRAJECTORYCONSTRUCT", minInputPorts = 1, maxInputPorts = 1, doc="Construct Trajectories", category={LogicalOperatorCategory.ADVANCED})
 public class TrajectoryConstructAO extends UnaryLogicalOp {
@@ -36,13 +39,17 @@ public class TrajectoryConstructAO extends UnaryLogicalOp {
 			TrajectoryConstructAO.class.getName(), 
 			Tuple.class, 
 			new SDFAttribute(null, VEHICLE_ID_ATTR_NAME, SDFDatatype.STRING, null),
-			new SDFAttribute(null, START_TIMESTAMP_ATTR_NAME, SDFDatatype.STRING, null),
-			new SDFAttribute(null, END_TIMESTAMP_ATTR_NAME, SDFDatatype.STRING, null),
+//			new SDFAttribute(null, START_TIMESTAMP_ATTR_NAME, SDFDatatype.STRING, null),
+//			new SDFAttribute(null, END_TIMESTAMP_ATTR_NAME, SDFDatatype.STRING, null),
 			new SDFAttribute(null, POINTS_ATTR_NAME, SDFSpatialDatatype.LIST , null)
 	);
 	
 	
 	private boolean subtrajectories = false;
+	
+	private SDFAttribute trajectoryId;
+	
+	private NestAggregateItem positionMapping;
 	
 	
 	public TrajectoryConstructAO() {
@@ -56,28 +63,47 @@ public class TrajectoryConstructAO extends UnaryLogicalOp {
 		super(trajectoryConstructAO);
 		
 		this.subtrajectories = trajectoryConstructAO.subtrajectories;
+		this.trajectoryId = trajectoryConstructAO.trajectoryId;
+		this.positionMapping = trajectoryConstructAO.positionMapping;
 	}
 	
 	
 //#######################################
 	// Own parameters
 	
-	@Parameter(name = "SUBTRAJECTORIES", type = BooleanParameter.class, isList = false, optional = true)
+	@Parameter(name = "SUBTRAJECTORIES", type = BooleanParameter.class)
 	public void setSubtrajectories(final boolean subtrajectories) {
 		this.subtrajectories = subtrajectories;
 	}
+	
+	@Parameter(name = "TRAJECTORYID", type = ResolvedSDFAttributeParameter.class)
+	public void setTrajectoryId(final SDFAttribute trajectoryId) {
+		this.trajectoryId = trajectoryId;
+	}
+	
+	@Parameter(name = "POSITIONMAP", type = NestAggregateItemParameter.class)
+	public void setPositionMapping(final NestAggregateItem positionMapping) {
+		this.positionMapping = positionMapping;
+	}
+	
 	
 	public boolean getSubtrajectories() {
 		return this.subtrajectories;
 	}
 	
-	
-	
-	
-	@Override
-	protected SDFSchema getOutputSchemaIntern(int pos) {
-		return OUTPUT_SCHEMA;
+	public SDFAttribute getTrajectoryId() {
+		return this.trajectoryId;
 	}
+	
+	public NestAggregateItem getPositionMapping() {
+		return this.positionMapping;
+	}
+	
+	
+//	@Override
+//	protected SDFSchema getOutputSchemaIntern(int pos) {
+//		return OUTPUT_SCHEMA;
+//	}
 	
 	
 	@Override
--- trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/transform/TTrajectoryContructAORule.java
+++ trajectory/de.uniol.inf.is.odysseus.trajectory/src/main/de/uniol/inf/is/odysseus/trajectory/transform/TTrajectoryContructAORule.java
@@ -1,8 +1,24 @@
 package de.uniol.inf.is.odysseus.trajectory.transform;
 
+import java.util.Arrays;
+import java.util.List;
+
 import de.uniol.inf.is.odysseus.core.collection.Tuple;
 import de.uniol.inf.is.odysseus.core.metadata.ITimeInterval;
+import de.uniol.inf.is.odysseus.core.sdf.schema.DirectAttributeResolver;
+import de.uniol.inf.is.odysseus.core.sdf.schema.IAttributeResolver;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFAttribute;
+import de.uniol.inf.is.odysseus.core.sdf.schema.SDFExpression;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.AggregateAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.PredicateWindowAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.RestructHelper;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.SelectAO;
+import de.uniol.inf.is.odysseus.core.server.logicaloperator.builder.AggregateItem;
 import de.uniol.inf.is.odysseus.core.server.planmanagement.TransformationConfiguration;
+import de.uniol.inf.is.odysseus.core.server.predicate.TruePredicate;
+import de.uniol.inf.is.odysseus.mep.MEP;
+import de.uniol.inf.is.odysseus.parser.pql.relational.RelationalPredicateBuilder;
+import de.uniol.inf.is.odysseus.relational.base.predicate.RelationalPredicate;
 import de.uniol.inf.is.odysseus.ruleengine.ruleflow.IRuleFlowGroup;
 import de.uniol.inf.is.odysseus.trajectory.logicaloperator.TrajectoryConstructAO;
 import de.uniol.inf.is.odysseus.trajectory.physical.TrajectoryConstructPO;
@@ -24,24 +40,54 @@ public class TTrajectoryContructAORule extends AbstractTransformationRule<Trajec
 	@Override
 	public void execute(TrajectoryConstructAO operator, TransformationConfiguration config) {
 		
-		final ITrajectoryConstructStrategy strategy = operator.getSubtrajectories() ?
-				new SubtrajectoryConstructStrategy() : new DefaultTrajectoryConstructStrategy();
-		this.defaultExecute(operator, new TrajectoryConstructPO<Tuple<ITimeInterval>>(strategy), config, true, true);
+		final List<SDFAttribute> groupOrPartitionBy = Arrays.asList(operator.getTrajectoryId());
+		
+		final AggregateAO aggregateAO = new AggregateAO();
+		aggregateAO.setName("TrajectoryConstruct Aggregate");
+		aggregateAO.setGroupingAttributes(groupOrPartitionBy);
+		aggregateAO.setAggregationItems(Arrays.asList((AggregateItem)operator.getPositionMapping()));
+		
+		RestructHelper.insertOperatorBefore(aggregateAO, operator);
+		this.insert(aggregateAO);
+		
+		if(!operator.getSubtrajectories()) {
+			final PredicateWindowAO predicateWindowAO = new PredicateWindowAO();
+			predicateWindowAO.setSameStarttime(true);
+			predicateWindowAO.setPartitionBy(groupOrPartitionBy);
+			
+			
+			IAttributeResolver r = new DirectAttributeResolver(aggregateAO.getInputSchema());
+			RelationalPredicateBuilder m = new RelationalPredicateBuilder();
+			
+			final RelationalPredicate p = (RelationalPredicate)m.createPredicate(r, "State = 0");
+			p.init(aggregateAO.getInputSchema(), aggregateAO.getInputSchema());
+			predicateWindowAO.setStartCondition(p);
+			
+			final RelationalPredicate p1 = (RelationalPredicate)m.createPredicate(r, "State = -1");
+			p1.init(aggregateAO.getInputSchema(), aggregateAO.getInputSchema());
+			predicateWindowAO.setEndCondition(p1);
+			
+			RestructHelper.insertOperatorBefore(predicateWindowAO, aggregateAO);
+			this.insert(predicateWindowAO);
+		}
+	
+		RestructHelper.removeOperator(operator, false);
+		this.retract(operator);
 	}
 
 	@Override
 	 public boolean isExecutable(TrajectoryConstructAO operator, TransformationConfiguration transformConfig) {
-		 return operator.isAllPhysicalInputSet();
+		 return true;
 	 }
 	 
 	 @Override
 	 public String getName() {
-		 return "TrajectoryConstructAO -> TrajectoryConstructPO";
+		 return "TrajectoryConstructAO -> PO";
 	 }
 	  
 	 @Override
 	 public IRuleFlowGroup getRuleFlowGroup() {
-		 return TransformRuleFlowGroup.TRANSFORMATION;
+		 return TransformRuleFlowGroup.SUBSTITUTION;
 	 }
 
 }
